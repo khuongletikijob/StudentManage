4023325ce64850ab65f4806858b0a582eefb42a7
khuongletikijob <131225699+khuongletikijob@users.noreply.github.com>
2024-10-08 16:56:58 +0700

    feat: student manage

--- StudentManage/Controllers/StudentController.cs
+++ StudentManage/Controllers/StudentController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using StudentManage.Data;
 using StudentManage.Models;
 using System.Security.Claims;
@@ -22,33 +23,31 @@ namespace StudentManage.Controllers
         public IActionResult Index()
         {
             var students = _context.S_Students.ToList();
-            var user = HttpContext.User;
-
-            Console.WriteLine("user=========================");
-            Console.WriteLine(user.FindFirst(ClaimTypes.Name)?.Value);
 
             return View(students);
         }
 
-        [HttpGet]
+        [HttpGet("Create")]
         public IActionResult Create()
         {
             return View();
         }
 
-        [HttpPost]
-        public IActionResult Create(S_Student student)
+        [HttpPost("Create")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(S_Student student)
         {
             if (ModelState.IsValid)
             {
-                _context.S_Students.Add(student);
-                _context.SaveChanges();
-                return RedirectToAction("Index");
+                _context.Add(student);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
             }
             return View(student);
         }
 
-        [HttpGet]
+
+        [HttpGet("Edit")]
         public IActionResult Edit(int id)
         {
             var student = _context.S_Students.SingleOrDefault(s => s.Id == id);
@@ -59,15 +58,53 @@ namespace StudentManage.Controllers
             return View(student);
         }
 
-        [HttpPost]
-        public IActionResult Edit(S_Student student)
+
+        [HttpPost("Edit")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Phone,Class")] S_Student student)
         {
+            if (id != student.Id)
+            {
+                return NotFound();
+            }
+
             if (ModelState.IsValid)
             {
-                _context.S_Students.Update(student);
-                _context.SaveChanges();
-                return RedirectToAction("Index");
+                // Fetch the original entity from the database
+                var existingStudent = await _context.S_Students.FindAsync(id);
+
+                if (existingStudent == null)
+                {
+                    return NotFound();
+                }
+
+                // Update the properties
+                existingStudent.Name = student.Name;
+                existingStudent.Phone = student.Phone;
+                existingStudent.Class = student.Class;
+
+                try
+                {
+                    // Save the changes
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.S_Students.Any(e => e.Id == student.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+
+                // Redirect back to the list of students after a successful update
+                return RedirectToAction(nameof(Index));
             }
+
+            // Return the view with the same student data in case of validation errors
             return View(student);
         }
 
--- StudentManage/Models/S_Student.cs
+++ StudentManage/Models/S_Student.cs
@@ -1,13 +1,18 @@
-﻿namespace StudentManage.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StudentManage.Models
 {
     public class S_Student
     {
         public int Id { get; set; }
 
+        [Required]
         public string Name { get; set; }
 
+        [Required]
         public string Phone { get; set; }
 
+        [Required]
         public string Class { get; set; }
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
